fcd55bdd6c83134286e4bd9ba15e9d77002970ab
David Schulz <deschulz22@gmail.com>
2020-02-20 15:00:52 -0600

    Gather command revised

--- src/main/java/gr3enmachin3/rosiemod/ChatHandler.java
+++ src/main/java/gr3enmachin3/rosiemod/ChatHandler.java
@@ -9,68 +9,81 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod.EventBusSubscriber
 public class ChatHandler {
+    private static Minecraft mc = Minecraft.getMinecraft();
+    private static ICommandManager baritone = BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager();
+
+    private static String requester;
+
+    private static boolean isGetActive;
+    private static int bringBlockId;
+    private static int desiredAmount;
+
+    @SubscribeEvent
+    public static void tick(TickEvent.PlayerTickEvent event) {
+        if (isGetActive) {
+            Item item = Item.getItemById(bringBlockId);
+            ItemStack itemStack = new ItemStack(item);
+            int slot = mc.player.inventory.getSlotFor(itemStack);
+            if (mc.player.inventory.getStackInSlot(slot).getCount() >= desiredAmount) {
+                isGetActive = false;
+                bringBack(slot, itemStack);
+            }
+        }
+    }
+
     @SubscribeEvent
     public static void readChat(ClientChatReceivedEvent event) {
-        Minecraft mc = Minecraft.getMinecraft();
-        ICommandManager baritone = BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager();
-
         String chat = event.getMessage().getUnformattedText();
         if (!chat.contains(">")) return;
 
-        String player = chat.substring(1, chat.indexOf(">"));
+        requester = chat.substring(1, chat.indexOf(">"));
         String message = chat.substring(chat.indexOf(">") + 1).toLowerCase();
 
-        String command = "";
-
-        if (!player.equals(mc.player.getName()) && message.contains("rosie")) {
+        if (!requester.equals(mc.player.getName()) && message.contains("rosie")) {
             if (message.contains("follow me")) {
-                mc.player.sendChatMessage("Ok, " + player + ", I will follow you.");
-                command += "follow player " + player;
-                baritone.execute(command);
+                mc.player.sendChatMessage("Ok, " + requester + ", I will follow you.");
+                baritone.execute("follow player " + requester);
                 return;
             }
 
             if (message.contains("get") || message.contains("bring")) {
-                command += "mine ";
+                String command = "mine ";
                 if (message.contains("wood") || message.contains("logs")) {
-                    mc.player.sendChatMessage("Ok, " + player + ", I'll be back with what you need.");
+                    mc.player.sendChatMessage("Ok, " + requester + ", I'll be back with what you need.");
 
+                    bringBlockId = 17;
                     command += "log ";
 
                     String num = message.replaceAll("\\D+","");
-                    int amount;
                     if (!num.equals("")) {
-                        amount = Integer.parseInt(num);
+                        desiredAmount = Integer.parseInt(num);
                     } else {
-                        amount = 16;
+                        desiredAmount = 16;
                     }
-                    command += amount;
+                    command += desiredAmount;
+
                     baritone.execute(command);
-
-                    Item log = Item.getItemById(17);
-                    ItemStack logStack = new ItemStack(log);
-                    int slot = mc.player.inventory.getSlotFor(logStack);
-                    while (mc.player.inventory.getStackInSlot(slot).getCount() < amount) {
-                        // Do nothing
-                    }
-
-                    mc.player.sendChatMessage("/tp " + player);
-                    mc.player.sendChatMessage("Here you go, " + player + ".");
-                    EntityItem logEntity = new EntityItem(mc.player.world, mc.player.posX, mc.player.posY+1, mc.player.posZ, logStack);
-                    logEntity.setDefaultPickupDelay();
-                    mc.player.world.spawnEntity(logEntity);
-                    mc.player.inventory.setInventorySlotContents(slot, null);
+                    isGetActive = true;
                 }
                 return;
             }
 
             if (message.contains("stop")) {
-                command = "cancel";
-                baritone.execute(command);
+                baritone.execute("stop");
             }
         }
+    }
+
+    private static void bringBack(int slot, ItemStack stack) {
+        mc.player.sendChatMessage("/tp " + requester);
+        mc.player.sendChatMessage("Here you go, " + requester + ".");
+        EntityItem logEntity = new EntityItem(mc.player.world, mc.player.posX, mc.player.posY+1, mc.player.posZ, stack);
+        logEntity.setDefaultPickupDelay();
+        mc.player.world.spawnEntity(logEntity);
+        mc.player.inventory.setInventorySlotContents(slot, null);
     }
 }
